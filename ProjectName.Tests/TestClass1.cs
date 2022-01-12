76d73f705c0f221257502cac99361fdd5bd4ca7e
Mikael Dúi Bolinder <github@bolinder.uk>
2022-01-13 00:57:50 +0100

    Update TestClass1.cs

--- ProjectName.Tests/TestClass1.cs
+++ ProjectName.Tests/TestClass1.cs
@@ -1,4 +1,10 @@
-namespace {{Namespace}}
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace {{Namespace}}.Tests
 {
     [TestClass]
     public class TestClass1
